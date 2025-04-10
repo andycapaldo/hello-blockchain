e3fb29884782c9e34120682f5a4d04ef7f9ca5c6
Andrew Capaldo <andycapaldo2@gmail.com>
2025-04-10 13:21:08 -0600

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,106 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useAccount, useReadContracts } from 'wagmi';
+import { formatUnits } from 'viem';
+import { ERC20_ABI } from '../lib/erc20';
+import { useState } from 'react';
+
+
+
+
+
+
+
+
+export default function HomePage() {
+  const { address, isConnected } = useAccount();
+
+  const [contractAddress, setContractAddress] = useState('');
+  const [submittedAddress, setSubmittedAddress] = useState<string | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSubmittedAddress(contractAddress);
+  }
+
+  const { data, isLoading, error } = useReadContracts({
+    contracts:
+      submittedAddress && address
+        ? [
+            {
+              address: submittedAddress as `0x${string}`,
+              abi: ERC20_ABI,
+              functionName: 'balanceOf',
+              args: [address!],
+            },
+            {
+              address: submittedAddress as `0x${string}`,
+              abi: ERC20_ABI,
+              functionName: 'decimals',
+            },
+            {
+              address: submittedAddress as `0x${string}`,
+              abi: ERC20_ABI,
+              functionName: 'symbol',
+            },
+          ]
+        : [],
+    query: {
+      enabled: !!submittedAddress && !!address,
+    },
+  });
+
+  const hasAllData =
+    data &&
+    data[0]?.status === 'success' &&
+    data[1]?.status === 'success' &&
+    data[2]?.status === 'success'
+    
+  const formattedBalance = hasAllData
+    ? formatUnits(data[0].result as bigint, data[1].result as number)
+    : '0';
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen flex flex-col items-center justify-center p-8 bg-blue-100 text-blue-800">
+      <h1 className="text-3xl font-bold mb-4">👋 Hello, Blockchain!</h1>
+      <div className='my-4'>
+        <ConnectButton />
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      {isConnected && (
+        <>
+          <form onSubmit={handleSubmit} className='mt-6 flex flex-col items-center gap-2 border-2 border-blue-200 p-4 rounded-lg w-1/5'>
+            <input 
+              type='text'
+              placeholder='Enter contract address'
+              value={contractAddress}
+              onChange={(e) => setContractAddress(e.target.value)}
+              className='p-2 rounded text-sm text-black w-full outline-none text-center'
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+            <div className='bg-blue-400 hover:bg-blue-700'>
+              <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded-lg cursor-pointer">
+                Check Balance
+              </button>
+            </div>
+          </form>
+
+          <div className="mt-6 text-center">
+            {isLoading ? (
+              <p>Loading token balance...</p>
+            ) : error ? (
+              <p>Error fetching token balance</p>
+            ) : hasAllData ? (
+              <p>
+                💰 Token Balance: {formattedBalance} {String(data[2].result)}
+              </p>
+            ) : submittedAddress ? (
+              <p>Fetching balance...</p>
+            ) : null}
+          </div>
+        </>
+      )}
+  </div>
+  )
 }
